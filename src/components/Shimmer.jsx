27a084be8a2b0af799705169a43cd17b0ee90d72
Aakarshit07 <aakarshit070@gmail.com>
2024-05-04 03:24:15 +0530

    loading screen fizxed

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,7 +1,7 @@
 
 export const Loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-[90vh]">
       <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-purple-500"></div>
       <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-red-500 ml-3"></div>
       <div className="animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-blue-500 ml-3"></div>
